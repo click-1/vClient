5a049b004027b2514163e2758e6d60c9a6c0bcac
click-1 <victorpak93@gmail.com>
2021-12-04 14:58:58 -0800

    updated NoSlow watchdog bypass

--- src/minecraft/com/vClient/module/movement/NoSlow.java
+++ src/minecraft/com/vClient/module/movement/NoSlow.java
@@ -4,10 +4,9 @@ import com.vClient.event.EventTarget;
 import com.vClient.event.events.EventPreMotionUpdate;
 import com.vClient.module.Category;
 import com.vClient.module.Module;
+import com.vClient.util.MovementUtil;
 import net.minecraft.item.ItemSword;
-import net.minecraft.network.play.client.C07PacketPlayerDigging;
-import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import org.lwjgl.input.Keyboard;
 
 public class NoSlow extends Module {
@@ -16,8 +15,8 @@ public class NoSlow extends Module {
     }
     @EventTarget
     public void onPre(EventPreMotionUpdate event) {
-        if (mc.thePlayer.onGround && mc.thePlayer.getItemInUse().getItem() instanceof ItemSword) {
-            mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN));
+        if (mc.thePlayer.onGround && MovementUtil.isMoving() && mc.thePlayer.getItemInUse().getItem() instanceof ItemSword) {
+            mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()));
         }
     }
 }
